d0675a51019e5fc025f9396cca398a753ffb6237
Vasanth2001 <65166839+Vasanth2001@users.noreply.github.com>
2024-07-18 09:29:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import json
 from charm.toolbox.pairinggroup import PairingGroup, GT
 from ABE.ac17 import AC17CPABE
 from file_abe import file_to_group_element, group_element_to_file
 
 def main():
+    # Load configuration
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+
+    # Extract configuration
+    input_file_path = config['input_file_path']
+    output_encrypted_file_path = config['output_encrypted_file_path']
+    output_decrypted_file_path = config['output_decrypted_file_path']
+    attributes = config['attributes']
+    policy_str = config['policy']
+
     # Instantiate a bilinear pairing map
     pairing_group = PairingGroup('MNT224')
 
@@ -13,19 +25,12 @@ def main():
     (pk, msk) = cpabe.setup()
 
     # Generate a key
-    attr_list = ['ONE', 'TWO', 'THREE']
-    key = cpabe.keygen(pk, msk, attr_list)
-
-    # File paths
-    input_file_path = 'path/to/your/input_file.txt'
-    output_encrypted_file_path = 'path/to/your/encrypted_file.enc'
-    output_decrypted_file_path = 'path/to/your/decrypted_file.txt'
+    key = cpabe.keygen(pk, msk, attributes)
 
     # Convert the file contents to a group element
     file_element = file_to_group_element(pairing_group, input_file_path)
 
     # Generate a ciphertext
-    policy_str = '((ONE and THREE) and (TWO OR FOUR))'
     ctxt = cpabe.encrypt(pk, file_element, policy_str)
 
     # Decrypt the file contents
